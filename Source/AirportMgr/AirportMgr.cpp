4656364f084bc81ce4b38948cd040502464533db
DrFox <daren.fox@gmail.com>
2026-08-28 10:45:11 +0100

    Initial commit: blank UE 5.4 C++ project, gitignore, reference screenshot

---
+++ Source/AirportMgr/AirportMgr.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AirportMgr.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AirportMgr, "AirportMgr" );
